32c53c97673bc9a2a11289c173e0854bd2873536
salam <salam.tamimi99@gmail.com>
2023-08-22 19:22:01 +0300

    edits

--- pages/conn.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$dbServername= "localhost";
-$dbUsername= "root";
-$dbPassword= "";
-$dbName= "mobile_tech";
-
-$conn= new PDO('mysql:host=localhost;dbname=mobile_tech',$dbUsername,$dbPassword);
-?>
